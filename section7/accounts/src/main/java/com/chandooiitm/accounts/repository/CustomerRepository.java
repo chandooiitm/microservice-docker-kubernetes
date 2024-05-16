b859798868bbf62bfdf9ee3f48b1fec5459c6bb2
chandooiitm <chandoo_iitm@yahoo.co.in>
2024-05-16 12:12:31 +0530

    section 7 base version file

---
+++ section7/accounts/src/main/java/com/chandooiitm/accounts/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.chandooiitm.accounts.repository;
+
+import com.chandooiitm.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+
+
+}
